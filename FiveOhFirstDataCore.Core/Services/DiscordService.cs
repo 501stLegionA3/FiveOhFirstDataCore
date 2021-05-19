435b609d49526efc6a45d45d865eb5068fcda188
Andrew Bounds <andybounds@gmail.com>
2021-05-19 01:11:09 -0400

    Added discord services

---
+++ FiveOhFirstDataCore.Core/Services/DiscordService.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveOhFirstDataCore.Core.Services
+{
+    public class DiscordService : IDiscordService
+    {
+        private readonly DiscordRestClient _rest;
+
+        public DiscordService(DiscordRestClient rest)
+        {
+            _rest = rest;
+        }
+
+
+    }
+}
