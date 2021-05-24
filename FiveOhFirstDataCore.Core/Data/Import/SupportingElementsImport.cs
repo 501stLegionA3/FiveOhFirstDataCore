63fa99d1c91cb43778c20368ff59c0d004d067a4
Andrew Bounds <andybounds@gmail.com>
2021-05-24 18:08:48 -0400

    Added supporting elements import and disabled button click during import.

--- FiveOhFirstDataCore.Core/Data/Import/SupportingElementsImport.cs
+++ FiveOhFirstDataCore.Core/Data/Import/SupportingElementsImport.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FiveOhFirstDataCore.Core.Data.Import
 {
-    public class SupportingElementsImport
+    public class SupportingElementsImport : IDisposable, IAsyncDisposable
     {
+        private bool disposedValue;
+
+        public FileStream? InactiveReservesStream { get; set; }
+        public FileStream? TrainingStream { get; set; }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    InactiveReservesStream?.Dispose();
+                    TrainingStream?.Dispose();
+                }
+
+                InactiveReservesStream = null;
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual async ValueTask DisposeAsyncCore()
+        {
+            if (InactiveReservesStream is not null)
+                await InactiveReservesStream.DisposeAsync();
+
+            if (TrainingStream is not null)
+                await TrainingStream.DisposeAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeAsyncCore();
+
+            Dispose(false);
+            GC.SuppressFinalize(this);
+        }
     }
 }
