f2566f9ed223e5fee0251b6e91616bea084ef53b
Andrew Bounds <andybounds@gmail.com>
2021-07-10 22:58:33 -0400

    Added option for links to open in new tab for text editor

--- FiveOhFirstDataCore/wwwroot/js/site.js
+++ FiveOhFirstDataCore/wwwroot/js/site.js
@@ -68,6 +68,18 @@
 							'tableProperties'
 						]
 					},
+					link: {
+						decorators: {
+							openInNewTab: {
+								mode: 'manual',
+								label: 'Open in a new tab',
+								attributes: {
+									target: '_blank',
+									rel: 'noopener noreferrer'
+								}
+							}
+						}
+					},
 					licenseKey: '',
 
 
