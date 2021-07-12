2fbfb54463020e3542b1bdbfe351468590263ab4
Andrew Bounds <andybounds@gmail.com>
2021-07-12 19:54:22 -0400

    Added promotions service interface

---
+++ FiveOhFirstDataCore.Core/Services/IPromotionService.cs
@@ -0,0 +1,22 @@
+﻿using FiveOhFirstDataCore.Core.Account;
+using FiveOhFirstDataCore.Core.Data.Promotions;
+using FiveOhFirstDataCore.Core.Structures;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveOhFirstDataCore.Core.Services
+{
+    public interface IPromotionService
+    {
+        public Task<Promotion> StartPromotionProcess(ClaimsPrincipal invoker, Trooper promotionFor,
+            int promotionFrom, int promotionTo);
+        public Task<ResultBase> ElevatePromotion(Promotion promotion, int levels = 1);
+        public Task<ResultBase> FinalizePromotion(Promotion promotion);
+        public Task<ResultBase> CancelPromotion(Promotion promotion);
+    }
+}
