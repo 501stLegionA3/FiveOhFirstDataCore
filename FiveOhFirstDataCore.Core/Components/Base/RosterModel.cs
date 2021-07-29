f3b3993cf25f7a793eff4f2a559f58ed41f431a1
Andrew Bounds <andybounds@gmail.com>
2021-07-29 11:34:37 -0400

    Added trooper page links to ORBAT and Zeta ORBAT. Closes #124

---
+++ FiveOhFirstDataCore.Core/Components/Base/RosterModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveOhFirstDataCore.Core.Components.Base
+{
+    public class RosterModel : ComponentBase
+    {
+        [Inject]
+        private NavigationManager _nav { get; set; }
+
+        public void TrooperNav(int? id)
+        {
+            if (id is not null)
+                _nav.NavigateTo($"/trooper/{id.Value}");
+        }
+    }
+}
