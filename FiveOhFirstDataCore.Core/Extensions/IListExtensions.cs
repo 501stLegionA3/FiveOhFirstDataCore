cdcd8afb88915f7d9de7ac9e9c35b3550c6343fe
Andrew Bounds <andybounds@gmail.com>
2021-08-04 19:49:12 -0400

    Gave access for RTOs and Medics to promote within their CoC

---
+++ FiveOhFirstDataCore.Core/Extensions/IListExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveOhFirstDataCore.Core.Extensions
+{
+    public static class IListExtensions
+    {
+        public static HashSet<T> ToHashSet<TValue, T>(this IEnumerable<TValue> value, Expression<Func<TValue, T>> selector) where TValue : class
+        {
+            var set = new HashSet<T>();
+            var func = selector.Compile();
+            foreach (var item in value)
+            {
+                set.Add(func.Invoke(item));
+            }
+            return set;
+        }
+
+        public static List<T> ToList<TValue, T>(this IEnumerable<TValue> value, Expression<Func<TValue, T>> selector) where TValue : class
+        {
+            var set = new List<T>();
+            var func = selector.Compile();
+            foreach (var item in value)
+            {
+                set.Add(func.Invoke(item));
+            }
+            return set;
+        }
+    }
+}
