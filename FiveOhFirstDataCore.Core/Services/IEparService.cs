62833824492116406943abbe30b3c437a30eae58
Andrew Bounds <andybounds@gmail.com>
2021-08-17 19:44:56 -0400

    Started work on the EPAR service.

---
+++ FiveOhFirstDataCore.Core/Services/IEparService.cs
@@ -0,0 +1,25 @@
+﻿using FiveOhFirstDataCore.Core.Account;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveOhFirstDataCore.Core.Services
+{
+    public interface IEparService
+    {
+        /// <summary>
+        /// Create a new change request and save it to the database.
+        /// </summary>
+        /// <returns>A task for this action.</returns>
+        public Task CreateChangeRequest();
+        /// <summary>
+        /// Get all active change requets.
+        /// </summary>
+        /// <returns>A task for this action that returns a <see cref="IReadOnlyList{T}"/> of type
+        /// <see cref="TrooperChangeRequestData"/></returns>
+        public Task<IReadOnlyList<TrooperChangeRequestData>> GetActiveChangeRequests();
+    }
+}
---
+++ FiveOhFirstDataCore/Pages/Personnel/Roster/EparApproval.razor.cs
@@ -0,0 +1,21 @@
+﻿using FiveOhFirstDataCore.Core.Account;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiveOhFirstDataCore.Pages.Personnel.Roster
+{
+    public partial class EparApproval
+    {
+        protected List<TrooperChangeRequestData> ChangeRequests { get; set; } = new();
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+
+        }
+    }
+}
