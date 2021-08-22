223f24fe75e276ae1a83bbe6737ca9c844cd98a1
Andrew Bounds <andybounds@gmail.com>
2021-08-22 14:48:09 -0400

    Added roster directories. Closes #178

---
+++ FiveOhFirstDataCore/Pages/Roster/Directory/CompanyDirectory.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveOhFirstDataCore.Pages.Roster.Directory
+{
+    public partial class CompanyDirectory
+    {
+        public List<(string, string)> Urls { get; set; } = new() { ("/", "Home"), ("/roster", "Roster"), ("/roster/directory", "Directories"),
+            ("/roster/directory/company", "Company Directory") };
+        private string Search { get; set; } = "";
+        private string SearchDummy { get; set; } = "";
+
+        private void SearchUpdate(ChangeEventArgs e)
+        {
+            Search = (string?)e?.Value ?? "";
+        }
+    }
+}
---
+++ FiveOhFirstDataCore/Pages/Roster/Directory/SquadDirectory.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveOhFirstDataCore.Pages.Roster.Directory
+{
+    public partial class SquadDirectory
+    {
+        public List<(string, string)> Urls { get; set; } = new()
+        {
+            ("/", "Home"),
+            ("/roster", "Roster"),
+            ("/roster/directory", "Directories"),
+            ("/roster/directory/squad", "Squad Directory")
+        };
+        private string Search { get; set; } = "";
+        private string SearchDummy { get; set; } = "";
+
+        private void SearchUpdate(ChangeEventArgs e)
+        {
+            Search = (string?)e?.Value ?? "";
+        }
+    }
+}
