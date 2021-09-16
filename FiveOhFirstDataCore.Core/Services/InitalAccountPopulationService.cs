ab0dfffcdf59e9b03d23d54d26b9f60b90c99619
connor jones <cj.power5000@gmail.com>
2021-09-16 15:45:51 -0600

    added Imports.cs to each project for Global Using.

--- FiveOhFirstDataCore.Core/Services/InitalAccountPopulationService.cs
+++ FiveOhFirstDataCore.Core/Services/InitalAccountPopulationService.cs
@@ -1,10 +1,4 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.Threading;
-global using System.Threading.Tasks;
-global using System.Linq;
-
-using FiveOhFirstDataCore.Core.Account;
+﻿using FiveOhFirstDataCore.Core.Account;
 using FiveOhFirstDataCore.Core.Data;
 
 using Microsoft.AspNetCore.Identity;
