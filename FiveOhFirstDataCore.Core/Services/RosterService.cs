7bf35401dc4b04aa883696977b38c12bf7b90dc4
connor jones <cj.power5000@gmail.com>
2021-09-17 09:41:23 -0600

    Updated nuget Packages and fixed Typo in RosterService

--- FiveOhFirstDataCore.Core/Services/RosterService.cs
+++ FiveOhFirstDataCore.Core/Services/RosterService.cs
@@ -570,7 +570,6 @@ namespace FiveOhFirstDataCore.Core.Services
             if (manager || (slot >= Slot.Mynock && slot <= Slot.MynockOneThree))
             {
                 var data = new MynockSectionData();
-            var data = new MynockSectionData();
 
                 using var _dbContext = _dbContextFactory.CreateDbContext();
 
