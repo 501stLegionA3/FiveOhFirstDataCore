4eb7f3f7a74b212af9e94b182f1e11aff9ce0d75
Andrew Bounds <andybounds@gmail.com>
2021-09-18 20:57:39 -0400

    Fixed broken nameing

--- FiveOhFirstDataCore.Core/Migrations/20210918150810_Dev-Soyvolon-Policies.cs
+++ FiveOhFirstDataCore.Core/Migrations/20210918150810_Dev-Soyvolon-Policies.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace FiveOhFirstDataCore.Core.Migrations
+namespace FiveOhFirstDataCore.Data.Migrations
 {
     public partial class DevSoyvolonPolicies : Migration
     {
--- FiveOhFirstDataCore.Core/Structures/PolicySectionResult.cs
+++ FiveOhFirstDataCore.Core/Structures/PolicySectionResult.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FiveOhFirstDataCore.Core.Structures;
+namespace FiveOhFirstDataCore.Data.Structures;
 
 public class PolicySectionResult : ResultBase
 {
