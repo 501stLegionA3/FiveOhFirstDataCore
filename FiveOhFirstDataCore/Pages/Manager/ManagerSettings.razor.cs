cd0a11409e2893ab57bf29dca24d1fb67be2c19e
Travis Thoene <forbiddenchaos@gmail.com>
2021-09-26 12:28:36 -0500

    IsAcceptable
    Added two check boxes for some buttons as requested.

---
+++ FiveOhFirstDataCore/Pages/Manager/ManagerSettings.razor.cs
@@ -0,0 +1,28 @@
+﻿// Do I Need Too? - Cruisie
+
+namespace FiveOhFirstDataCore.Pages.Manager
+{
+    public partial class ManagerSettings
+    {
+        public List<(string, string)> Urls = new() { ("/", "Home"), ("/manager", "Manager Home"), ("/manager/settings", "Manager Settings") };
+
+        public enum ManagerDisplayOption
+        {
+            CShops,
+            Discord,
+            Promotions,
+            Settings,
+        }
+
+        public ManagerDisplayOption Active { get; set; }
+
+        private void OnTypeChange(ManagerDisplayOption option)
+        {
+            Active = option;
+            StateHasChanged();
+        }
+
+        protected bool IsAcknowledgedDWS { get; set; }
+        protected bool IsAcknowledgedVTR { get; set; }
+    }
+}
