4cd872601a7b75ba683c29ac720a4cb3185bb5bc
Bounds <andybounds@gmail.com>
2021-11-03 13:06:05 -0400

    Setup inital tailwind css stuffs

---
+++ ProjectDataCore/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	purge: {
+		enabled: true,
+		content: [
+			'./**/*.html',
+			'./**/*.razor'
+		],
+	},
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+	extend: {},
+	},
+	variants: {
+	extend: {},
+	},
+	plugins: [],
+}
