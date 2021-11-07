1f8e57e702fd0d74bfa6d8d6b1fb34e7c394705b
Bounds <andybounds@gmail.com>
2021-11-07 10:40:48 -0500

    Added asiignable data service framework

---
+++ ProjectDataCore.Data/Services/Roster/IAssignableDataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDataCore.Data.Services.Roster;
+
+/// <summary>
+/// A service to handle the creation and management of Assignable data vlaues.
+/// </summary>
+public interface IAssignableDataService
+{
+
+}
