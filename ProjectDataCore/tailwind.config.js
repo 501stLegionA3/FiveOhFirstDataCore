2b4c29c3ea761c2e27980882c14d1e9028f21128
Bounds <andybounds@gmail.com>
2021-11-07 09:27:41 -0500

    updated jit

--- ProjectDataCore/tailwind.config.js
+++ ProjectDataCore/tailwind.config.js
@@ -1,12 +1,9 @@
 module.exports = {
+	mode: 'jit',
 	purge: {
 		enabled: true,
 		content: [
-			'../**/*.html',
-			'../**/*.razor',
-			'../**/*.razor.css',
-			'../**/*.cshtml',
-			'../**/*.cshtml.css'
+			'../**/*.{html,razor,razor.css,cshtml,cshtml.css}',
 		],
 	},
 	darkMode: false, // or 'media' or 'class'
