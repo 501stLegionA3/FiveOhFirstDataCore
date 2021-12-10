ee47f2f862d49fc6d062d8f2563f87f4978eb0f5
Bounds <andybounds@gmail.com>
2021-12-10 14:58:59 -0500

    Added layout parts.

---
+++ ProjectDataCore.Components/Parts/Layout/Form/MultiSelectMultiActionFormPart.razor.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using ProjectDataCore.Data.Services.Roster;
+using ProjectDataCore.Data.Structures;
+using ProjectDataCore.Data.Structures.Assignable;
+using ProjectDataCore.Data.Structures.Model;
+using ProjectDataCore.Data.Structures.Result;
+using ProjectDataCore.Data.Structures.Util;
+
+namespace ProjectDataCore.Components.Parts.Layout.Form
+{
+    public partial class MultiSelectMultiActionFormPart
+    {
+    }
+}
