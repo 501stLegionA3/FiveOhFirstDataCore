43378d5ba4ebc23380dd9cf48975294a35699e51
Bounds <andybounds@gmail.com>
2021-12-16 11:35:12 -0500

    put some inital work into the manager edit portion of the roster display.

---
+++ ProjectDataCore.Components/Parts/Roster/Parts/RosterDisplayManagerEdit.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDataCore.Components.Parts.Roster.Parts;
+public partial class RosterDisplayManagerEdit
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    [Inject]
+    public IPageEditService PageEditService { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    public RosterComponentSettingsEditModel RosterComponentSettings { get; set; } = new();
+}
