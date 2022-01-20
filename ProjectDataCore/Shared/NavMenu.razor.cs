2ee3c45dc4fb009e25a680be02f9c9e9206add67
Jacob Oxley <jdoxley6@gmail.com>
2022-01-19 18:04:12 -0800

    Add NavBar

--- ProjectDataCore/Shared/NavMenu.razor.cs
+++ ProjectDataCore/Shared/NavMenu.razor.cs
@@ -1,53 +1,39 @@
-﻿namespace ProjectDataCore.Shared;
+﻿using Microsoft.AspNetCore.Components.Routing;
+using ProjectDataCore.Data.Services.Nav;
+using ProjectDataCore.Data.Structures.Nav;
 
-public partial class NavMenu
+namespace ProjectDataCore.Shared;
+
+public partial class NavMenu : ComponentBase, IDisposable
 {
-    private List<Link> _modules = new List<Link>
-    {
-        new("Admin", "/admin", new List<Link>
-        {
-            new("Page Editor", "/admin/page/edit"),
-            new("Roster Editor", "/admin/roster/edit", new List<Link>
-            {
-                new("Roster List", "/admin/roster/list", new List<Link>
-                {
-                    new("Test", "/admin/roster/list/test")
-                }, true)
-            }, true)
-        })
-    };
+    private List<NavModule> _modules = new List<NavModule>();
+
+    public string URI { get; set; } = "";
 
     [Inject] public NavigationManager _navManager { get; set; }
+    [Inject] public INavModuleService _navModuleService { get; set; }
 
+    public void Dispose()
+    {
+        _navManager.LocationChanged -= LocationChanged;
+    }
+
+    protected async override Task OnInitializedAsync()
+    {
+        URI = _navManager.Uri;
+        _navManager.LocationChanged += LocationChanged;
+        _modules = await _navModuleService.GetAllModulesWithChildren();
+        
+    }
+
+    private void LocationChanged(object? sender, LocationChangedEventArgs e)
+    {
+        URI = e.Location;
+        InvokeAsync(StateHasChanged);
+    }
 
     private void Navigate(string href)
     {
         _navManager.NavigateTo(href, true);
     }
-}
-
-/// <summary>
-/// Used to represent modules for the top navigation bar
-/// </summary>
-public class Link
-{
-    public Link(string displayName, string href, bool hasMainPage = false)
-    {
-        DisplayName = displayName;
-        Href = href;
-        HasMainPage = hasMainPage;
-    }
-
-    public Link(string displayName, string href, List<Link> subLinks, bool hasMainPage = false)
-    {
-        DisplayName = displayName;
-        Href = href;
-        SubLinks = subLinks;
-        HasMainPage = hasMainPage;
-    }
-
-    public string DisplayName { get; set; }
-    public string Href { get; set; }
-    public List<Link>? SubLinks { get; set; }
-    public bool HasMainPage { get; set; } = false;
 }
