e23fbb3b814eda6756d9a524057de30ed52f0ebe
Jacob Oxley <jdoxley6@gmail.com>
2022-01-22 23:32:20 -0800

    Add Alert component

---
+++ ProjectDataCore.Components/Alert/Alert.razor.cs
@@ -0,0 +1,31 @@
+﻿using ProjectDataCore.Data.Structures.Model.Alert;
+using ProjectDataCore.Data.Services.Alert;
+
+namespace ProjectDataCore.Components.Alert
+{
+    public partial class Alert : ComponentBase
+    {
+        [Parameter]
+        public AlertModel AlertItem { get; set; }
+
+        [Inject]
+        public IAlertService AlertService { get; set; }
+
+        private string _alertColor { get {
+                switch (AlertItem.AlertType)
+                {
+                    case AlertType.Success: return "Success";
+                    case AlertType.Warn: return "Warn";
+                    case AlertType.Error: return "Error";
+                    case AlertType.Info: return "Info";
+                    default: return "";
+                }
+            } }
+
+
+    private void CloseAlert()
+        {
+            AlertService.DeleteAlert(AlertItem.Id);
+        }
+    }
+}
