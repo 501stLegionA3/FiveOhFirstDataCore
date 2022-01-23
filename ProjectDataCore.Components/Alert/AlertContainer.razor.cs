c31ddb5952c37774c32177de1eb1903818dd27f7
Jacob Oxley <jdoxley6@gmail.com>
2022-01-22 23:32:28 -0800

    Add alert container

---
+++ ProjectDataCore.Components/Alert/AlertContainer.razor.cs
@@ -0,0 +1,31 @@
+﻿using ProjectDataCore.Data.Services.Alert;
+using ProjectDataCore.Data.Structures.Model.Alert;
+
+namespace ProjectDataCore.Components.Alert
+{
+    public partial class AlertContainer : ComponentBase, IDisposable
+    {
+        [Inject]
+        public IAlertService AlertService { get; set; }
+
+        private List<AlertModel> _alerts = new();
+
+        protected async override Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            _alerts = AlertService.Alerts;
+            AlertService.AlertsChanged += AlertsChanged;
+        }
+
+        public void AlertsChanged(object sender, EventArgs e)
+        {
+            _alerts = AlertService.Alerts;
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void Dispose()
+        {
+            AlertService.AlertsChanged -= AlertsChanged;
+        }
+    }
+}
