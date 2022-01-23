564a40004f919f2123945691847819d16dd4bc47
Jacob Oxley <jdoxley6@gmail.com>
2022-01-22 23:31:53 -0800

    Add Alert Service

---
+++ ProjectDataCore.Data/Services/Alert/AlertService.cs
@@ -0,0 +1,62 @@
+﻿using ProjectDataCore.Data.Structures.Model.Alert;
+using System.Timers;
+
+namespace ProjectDataCore.Data.Services.Alert
+{
+    public class AlertService : IAlertService
+    {
+        public List<AlertModel> Alerts { get; set; } = new();
+        public event EventHandler AlertsChanged;
+
+        public ActionResult CreateAlert(string message, AlertType alertType, int duration)
+        {
+            AlertModel alertModel = new(alertType, message);
+            Alerts.Add(alertModel);
+            System.Timers.Timer timer = new(duration);
+            timer.Elapsed += (sender, e)=>DeleteAlert(alertModel.Id);
+            //timer.Start(); TODO: Renable
+            return TriggerEvent();
+        }
+
+        public ActionResult CreateErrorAlert(string message, int duration = 3200)
+        {
+            return CreateAlert(message, AlertType.Error, duration);
+        }
+
+        public ActionResult CreateInfoAlert(string message, int duration = 3200)
+        {
+            return CreateAlert(message, AlertType.Info, duration);
+        }
+
+        public ActionResult CreateSuccessAlert(string message, int duration = 3200)
+        {
+            return CreateAlert(message, AlertType.Success, duration);
+        }
+
+        public ActionResult CreateWarnAlert(string message, int duration = 3200)
+        {
+            return CreateAlert(message, AlertType.Warn, duration);
+        }
+
+        public ActionResult DeleteAlert(Guid alertId)
+        {
+            Alerts.RemoveAll(e => e.Id.Equals(alertId));
+            return TriggerEvent();
+        }
+
+        internal ActionResult TriggerEvent()
+        {
+            try
+            {
+                AlertsChanged.Invoke(this, new EventArgs());
+            }
+            catch (Exception ex)
+            {
+                return new(false, new() { ex.Message});
+            }
+            return new(true);
+        }
+
+
+    }
+}
---
+++ ProjectDataCore.Data/Services/Alert/IAlertService.cs
@@ -0,0 +1,53 @@
+﻿using ProjectDataCore.Data.Structures.Model.Alert;
+
+namespace ProjectDataCore.Data.Services.Alert
+{
+    public interface IAlertService
+    {
+        public List<AlertModel> Alerts { get; set;}
+        public event EventHandler AlertsChanged;
+
+        /// <summary>
+        /// Generic method used to create a new alert of any type
+        /// </summary>
+        /// <param name="message">Message to be displayed</param>
+        /// <param name="alertType">One of <see cref="AlertType"/> to determine severity</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateAlert(string message, AlertType alertType, int duration);
+        /// <summary>
+        /// Create a Seccess Alert
+        /// </summary>
+        /// <param name="message">Message to be displayed</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateSuccessAlert(string message, int duration = 3200);
+        /// <summary>
+        /// Create an Error Alert
+        /// </summary>
+        /// <param name="message">Message to be displayed</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateErrorAlert(string message, int duration = 3200);
+        /// <summary>
+        /// Create an Info Alert
+        /// </summary>
+        /// <param name="message">Message to be displayed</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateInfoAlert(string message, int duration = 3200);
+        /// <summary>
+        /// Create a Warning Alert
+        /// </summary>
+        /// <param name="message">Message to be displayed</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateWarnAlert(string message, int duration = 3200);
+        /// <summary>
+        /// Delete an alert based on its Id
+        /// </summary>
+        /// <param name="alertId">The Id of the alert to delete.</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult DeleteAlert(Guid alertId);
+    }
+}
