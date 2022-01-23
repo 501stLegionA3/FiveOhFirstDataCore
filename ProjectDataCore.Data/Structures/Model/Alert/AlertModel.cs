6f5ee80d367a33ae06730b6b1f7c237a6412a970
Jacob Oxley <jdoxley6@gmail.com>
2022-01-22 23:31:53 -0800

    Add Alert Service

---
+++ ProjectDataCore.Data/Structures/Model/Alert/AlertModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDataCore.Data.Structures.Model.Alert
+{
+    public class AlertModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public AlertType AlertType { get; set; }
+        public string Message { get; set; } = "";
+
+        public AlertModel(AlertType alertType, string message)
+        {
+            AlertType = alertType;
+            Message = message;
+        }
+    }
+}
---
+++ ProjectDataCore.Data/Structures/Model/Alert/AlertType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDataCore.Data.Structures.Model.Alert
+{
+    public enum AlertType
+    {
+        Info,
+        Success,
+        Warn,
+        Error
+    }
+}
