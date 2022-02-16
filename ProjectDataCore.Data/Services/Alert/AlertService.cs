5a42103efe6ce931f0f33995a58b3e8d947de029
Jacob Oxley <jdoxley6@gmail.com>
2022-02-15 17:22:14 -0800

    Alert service (#357)
    * Add Alert Service
    
    * Register Alert Service
    
    * Add Alert component
    
    * Add alert container
    
    * Add alerts to the main layout
    
    * Update Alert Service and Model
    
    * Update Alert Component
    
    * Cleanup files
    
    * More changes to authentication.
    
    * Backend for dynamic policies are implemented.
    
    * Started work on dynamic policy editors
    
    * Add Alert Service
    
    * Register Alert Service
    
    * Add Alert component
    
    * Add alert container
    
    * Add alerts to the main layout
    
    * Update Alert Service and Model
    
    * Update Alert Component
    
    * Cleanup files
    
    * Fix requested changes
    
    * Helps to read all of the requests
    
    * Add Fluent API for alerts
    
    Co-authored-by: Bounds <andybounds@gmail.com>

---
+++ ProjectDataCore.Data/Services/Alert/AlertService.cs
@@ -0,0 +1,91 @@
+﻿using ProjectDataCore.Data.Structures.Model.Alert;
+using System.Timers;
+
+namespace ProjectDataCore.Data.Services.Alert
+{
+    public class AlertService : IAlertService
+    {
+        public List<AlertModel> Alerts { get; set; } = new();
+        public event EventHandler AlertsChanged;
+
+        public ActionResult CreateAlert(string message, AlertType alertType, bool enableTimer, int duration, bool trigger = true)
+        {
+            AlertModel alertModel = new(alertType, message, this, enableTimer, duration);
+            Alerts.Add(alertModel);
+            if (trigger)
+                return TriggerEvent();
+            return new(true);
+        }
+
+        public ActionResult AddAlert(AlertModel alert)
+        {
+            Alerts.Add(alert);
+            return TriggerEvent();
+        }
+
+        public ActionResult CreateErrorAlert(string message, bool enableTimer = false, int duration = 5000)
+        {
+            return CreateAlert(message, AlertType.Error, enableTimer, duration);
+        }
+
+        public ActionResult CreateErrorAlert(List<string> messages, bool enableTimer = false, int duration = 5000)
+        {
+            messages.ForEach(x => CreateAlert(x, AlertType.Error, enableTimer, duration, false));
+            return TriggerEvent();
+        }
+
+        public ActionResult CreateInfoAlert(string message, bool enableTimer = false, int duration = 3200)
+        {
+            return CreateAlert(message, AlertType.Info, enableTimer, duration);
+        }
+
+        public ActionResult CreateInfoAlert(List<string> messages, bool enableTimer = false, int duration = 3200)
+        {
+            messages.ForEach(x => CreateAlert(x, AlertType.Info, enableTimer, duration, false));
+            return TriggerEvent();
+        }
+
+        public ActionResult CreateSuccessAlert(string message, bool enableTimer = false, int duration = 3200)
+        {
+            return CreateAlert(message, AlertType.Success, enableTimer, duration);
+        }
+
+        public ActionResult CreateSuccessAlert(List<string> messages, bool enableTimer = false, int duration = 3200)
+        {
+            messages.ForEach(x => CreateAlert(x, AlertType.Success, enableTimer, duration, false));
+            return TriggerEvent();
+        }
+
+        public ActionResult CreateWarnAlert(string message, bool enableTimer = false, int duration = 3200)
+        {
+            return CreateAlert(message, AlertType.Warn, enableTimer, duration);
+        }
+        public ActionResult CreateWarnAlert(List<string> messages, bool enableTimer = false, int duration = 3200)
+        {
+            messages.ForEach(x => CreateAlert(x, AlertType.Warn, enableTimer, duration, false));
+            return TriggerEvent();
+        }
+
+        public ActionResult DeleteAlert(AlertModel alert)
+        {
+            alert.Dispose();
+            Alerts.Remove(alert);
+            return TriggerEvent();
+        }
+
+        internal ActionResult TriggerEvent()
+        {
+            try
+            {
+                AlertsChanged?.Invoke(this, new EventArgs());
+            }
+            catch (Exception ex)
+            {
+                return new(false, new() { ex.Message});
+            }
+            return new(true);
+        }
+
+
+    }
+}
---
+++ ProjectDataCore.Data/Services/Alert/IAlertService.cs
@@ -0,0 +1,101 @@
+﻿using ProjectDataCore.Data.Structures.Model.Alert;
+
+namespace ProjectDataCore.Data.Services.Alert
+{
+    public interface IAlertService
+    {
+        public List<AlertModel> Alerts { get; set;}
+        /// <summary>
+        /// Will trigger when a new alert is created.
+        /// </summary>
+        public event EventHandler AlertsChanged;
+
+        /// <summary>
+        /// Add an alert made with <see cref="IAlertBuilder"/>/
+        /// </summary>
+        /// <param name="alert">The alert to add.</param>
+        /// <returns>True/False depending on errors</returns>
+        public ActionResult AddAlert(AlertModel alert);
+
+        /// <summary>
+        /// Generic method used to create a new alert of any type
+        /// </summary>
+        /// <param name="message">Message to be displayed</param>
+        /// <param name="alertType">One of <see cref="AlertType"/> to determine severity</param>
+        /// <param name="enableTimer">If True the timer will be eneabled.</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <param name="trigger">Determines if creating the alert will trigger the <see cref="AlertsChanged"/> event.</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateAlert(string message, AlertType alertType, bool enableTimer, int duration, bool trigger = true);
+        /// <summary>
+        /// Create a Seccess Alert
+        /// </summary>
+        /// <param name="message">Message to be displayed</param>
+        /// <param name="enableTimer">If True the timer will be eneabled.</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateSuccessAlert(string message, bool enableTimer = false, int duration = 3200);
+        /// <summary>
+        /// Create a Seccess Alert
+        /// </summary>
+        /// <param name="messages">Messages to be displayed</param>
+        /// <param name="enableTimer">If True the timer will be eneabled.</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateSuccessAlert(List<string> messages, bool enableTimer = false, int duration = 3200);
+        /// <summary>
+        /// Create an Error Alert
+        /// </summary>
+        /// <param name="message">Message to be displayed</param>
+        /// <param name="enableTimer">If True the timer will be eneabled.</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateErrorAlert(string message, bool enableTimer = false, int duration = 5000);
+        /// <summary>
+        /// Create an Error Alert
+        /// </summary>
+        /// <param name="messages">Messages to be displayed</param>
+        /// <param name="enableTimer">If True the timer will be eneabled.</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateErrorAlert(List<string> messages, bool enableTimer = false, int duration = 5000);
+        /// <summary>
+        /// Create an Info Alert
+        /// </summary>
+        /// <param name="message">Message to be displayed</param>
+        /// <param name="enableTimer">If True the timer will be eneabled.</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateInfoAlert(string message, bool enableTimer = false, int duration = 3200);
+        /// <summary>
+        /// Create an Info Alert
+        /// </summary>
+        /// <param name="messages">Messages to be displayed</param>
+        /// <param name="enableTimer">If True the timer will be eneabled.</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateInfoAlert(List<string> messages, bool enableTimer = false, int duration = 3200);
+        /// <summary>
+        /// Create a Warning Alert
+        /// </summary>
+        /// <param name="message">Message to be displayed</param>
+        /// <param name="enableTimer">If True the timer will be eneabled.</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateWarnAlert(string message, bool enableTimer = false, int duration = 3200);
+        /// <summary>
+        /// Create a Warning Alert
+        /// </summary>
+        /// <param name="messages">Messages to be displayed</param>
+        /// <param name="enableTimer">If True the timer will be eneabled.</param>
+        /// <param name="duration">How long the alert will display in ms</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult CreateWarnAlert(List<string> messages, bool enableTimer = false, int duration = 3200);
+        /// <summary>
+        /// Delete an alert based on its Id
+        /// </summary>
+        /// <param name="alert">The alert to delete.</param>
+        /// <returns>True/False depedning on errors</returns>
+        public ActionResult DeleteAlert(AlertModel alert);
+    }
+}
