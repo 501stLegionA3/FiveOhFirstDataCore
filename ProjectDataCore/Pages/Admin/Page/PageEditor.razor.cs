a14468e6ab395d5591e89e222afa181e3b24e262
Bounds <andybounds@gmail.com>
2022-04-20 19:44:48 -0400

    Started work into the actual page building page.

--- ProjectDataCore/Pages/Admin/Page/PageEditor.razor.cs
+++ ProjectDataCore/Pages/Admin/Page/PageEditor.razor.cs
@@ -7,10 +7,30 @@ using System.Threading.Tasks;
 namespace ProjectDataCore.Pages.Admin.Page;
 public partial class PageEditor : ComponentBase
 {
+    #region States
+    public enum LeftMenu
+    {
+        PageSelection,
+        ComponentSelection,
+        SettingsSelection
+    }
+
+    public enum RightMenu
+    {
+        Empty,
+        PageEditor,
+        SettingsEditor
+    }
+    #endregion
+
 #pragma warning disable CS8618 // Inject is always non-null.
     [Inject]
     public IPageEditService PageEditService { get; set; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    public LeftMenu LeftMenuState { get; set; } = LeftMenu.PageSelection;
+    public RightMenu RightMenuState { get; set; } = RightMenu.Empty;
+
 
 
 }
