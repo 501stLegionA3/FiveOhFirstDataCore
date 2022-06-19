dfb19515f709ad829b742f3978c43ab00f4e8efb
Bounds <andybounds@gmail.com>
2022-06-19 14:30:47 -0400

    Fixed display errors.

--- ProjectDataCore.Components/Framework/Selectors/Settings/AuthorizationPolicySelector.razor.cs
+++ ProjectDataCore.Components/Framework/Selectors/Settings/AuthorizationPolicySelector.razor.cs
@@ -23,7 +23,7 @@ public partial class AuthorizationPolicySelector
 	public LayoutNode Settings { get; set; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-	[Parameter]
+	[CascadingParameter(Name = "PageEditComponent")]
 	public PageEditComponent? EditComponent { get; set; }
 
 	private List<DynamicAuthorizationPolicy> Policies { get; set; } = new();
