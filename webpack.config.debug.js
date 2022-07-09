4e64d2f8a0e0d5e0a030aef6d227ece150f29c33
Bounds <andybounds@gmail.com>
2022-07-09 19:01:32 -0400

    Monaco JS is setup and ready for further testing.

--- webpack.config.debug.js
+++ webpack.config.debug.js
@@ -2,6 +2,8 @@ const path = require('path');
 const root = path.resolve(__dirname, './ProjectDataCore/wwwroot');
 const dist = path.resolve(root, 'dist');
 
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = [
     {
         name: 'Site Utils',
@@ -36,6 +38,39 @@ module.exports = [
         output: {
             path: dist,
             filename: 'dropInterop.bundle.js',
+        },
+        mode: 'development',
+    },
+    {
+        name: 'Monaco Interop',
+        entry: {
+            app: path.resolve(root, './monaco/lightweightMonacoInterop.js'),
+            'editor.worker': 'monaco-editor/esm/vs/editor/editor.worker.js',
+            "json.worker": 'monaco-editor/esm/vs/language/json/json.worker',
+            "css.worker": 'monaco-editor/esm/vs/language/css/css.worker',
+            "html.worker": 'monaco-editor/esm/vs/language/html/html.worker',
+            "ts.worker": 'monaco-editor/esm/vs/language/typescript/ts.worker',
+        },
+        output: {
+            globalObject: 'self',
+            path: dist,
+            filename: 'lightweightMonacoInterop.bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.ttf$/,
+                    use: ['file-loader']
+                }
+            ]
+        },
+        optimization: {
+            minimize: true,
+            minimizer: [new TerserPlugin()]
         },
         mode: 'development',
     }
--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,80 @@ const path = require('path');
 const root = path.resolve(__dirname, './ProjectDataCore/wwwroot');
 const dist = path.resolve(root, 'dist');
 
-module.exports = [
-    {
-        name: 'Site Utils',
-        entry: path.resolve(root, './site.js'),
-        output: {
-            path: dist,
-            filename: 'site.bundle.js',
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = mode => {
+    return [
+        {
+            name: 'Site Utils',
+            entry: path.resolve(root, './site.js'),
+            output: {
+                path: dist,
+                filename: 'site.bundle.js',
+            },
+            mode: mode,
         },
-        mode: 'production',
-    },
-    {
-        name: 'CK Editor Interop',
-        entry: path.resolve(root, './ckeditor/interop/ckEditorInterop.js'),
-        output: {
-            path: dist,
-            filename: 'ckEditorInterop.bundle.js',
+        {
+            name: 'CK Editor Interop',
+            entry: path.resolve(root, './ckeditor/interop/ckEditorInterop.js'),
+            output: {
+                path: dist,
+                filename: 'ckEditorInterop.bundle.js',
+            },
+            mode: mode,
         },
-        mode: 'production',
-    },
-    {
-        name: 'Split Interop',
-        entry: path.resolve(root, './split/splitInterop.js'),
-        output: {
-            path: dist,
-            filename: 'splitInterop.bundle.js'
+        {
+            name: 'Split Interop',
+            entry: path.resolve(root, './split/splitInterop.js'),
+            output: {
+                path: dist,
+                filename: 'splitInterop.bundle.js'
+            },
+            mode: mode,
         },
-        mode: 'production',
-    },
-    {
-        name: 'Drop Interop',
-        entry: path.resolve(root, './drag/dropInterop.js'),
-        output: {
-            path: dist,
-            filename: 'dropInterop.bundle.js',
+        {
+            name: 'Drop Interop',
+            entry: path.resolve(root, './drag/dropInterop.js'),
+            output: {
+                path: dist,
+                filename: 'dropInterop.bundle.js',
+            },
+            mode: mode,
         },
-        mode: 'production',
-    }
-];
+        {
+            name: 'Monaco Interop',
+            entry: {
+                app: path.resolve(root, './monaco/lightweightMonacoInterop.js'),
+                "editor.worker": 'monaco-editor/esm/vs/editor/editor.worker.js',
+                "json.worker": 'monaco-editor/esm/vs/language/json/json.worker',
+                "css.worker": 'monaco-editor/esm/vs/language/css/css.worker',
+                "html.worker": 'monaco-editor/esm/vs/language/html/html.worker',
+                "ts.worker": 'monaco-editor/esm/vs/language/typescript/ts.worker',
+            },
+            output: {
+                globalObject: 'self',
+                path: path.resolve(dist, './monaco'),
+                filename: '[name].js',
+            },
+            mode: mode,
+            module: {
+                rules: [
+                    {
+                        test: /\.css$/,
+                        use: ['style-loader', 'css-loader']
+                    },
+                    {
+                        test: /\.ttf$/,
+                        use: ['file-loader']
+                    }
+                ]
+            },
+            optimization: {
+                minimize: true,
+                minimizer: [new TerserPlugin()]
+            }
+        }
+    ];
+};
 
 module.exports.parallelism = 1;
