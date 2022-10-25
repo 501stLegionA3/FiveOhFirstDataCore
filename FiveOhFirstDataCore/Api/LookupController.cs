64df0a1d4507f226747f5f2ee5909d1476819390
Thunder <jdoxley6@gmail.com>
2022-10-25 00:30:00 -0700

    change to csv, add cshop info

--- FiveOhFirstDataCore/Api/LookupController.cs
+++ FiveOhFirstDataCore/Api/LookupController.cs
@@ -1,8 +1,18 @@
 ﻿using FiveOhFirstDataCore.Data.Account;
+using FiveOhFirstDataCore.Data.Services;
 using FiveOhFirstDataCore.Data.Structures;
+using FiveOhFirstDataCore.Data.Structures.Updates;
 using FiveOhFirstDataCore.Data.Structuresbase;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Data;
+using System.Dynamic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace FiveOhFirstDataCore.Api;
 
@@ -15,7 +25,8 @@ public class FullUserDTO
     public string Slot { get; set; }
     public string Role { get; set; }
     public string Rank { get; set; }
-    public List<string> Qualifications { get; set; }
+    public string Qualifications { get; set; }
+    public string CShops { get; set; }
     public int TIG { get; set; }
     public int TIB { get; set; }
 }
@@ -31,23 +42,32 @@ public class UserDTO
 public class LookupController : ControllerBase
 {
     private readonly IDbContextFactory<ApplicationDbContext> _dbContextFactory;
+    private readonly IRosterService _roster;
 
-    public LookupController(IDbContextFactory<ApplicationDbContext> dbContextFactory)
+    public LookupController(IDbContextFactory<ApplicationDbContext> dbContextFactory, IRosterService roster)
     {
         _dbContextFactory = dbContextFactory;
+        _roster = roster;
     }
 
     [HttpGet("userinfo")]
-    [Produces("application/json")]
-    public async Task<ActionResult<FullUserDTO>> UserInfo([FromQuery] int user)
+    [Produces("text/plain")]
+    public async Task<ActionResult<string>> UserInfo([FromQuery] int user)
     {
         await using var dbContext = _dbContextFactory.CreateDbContext();
         var trooperQ = dbContext.Users.Where(e => e.BirthNumber == user);
-        if (await trooperQ.FirstOrDefaultAsync() == null)
-            return NotFound(user);
-        var qualifications = Enum.GetValues<Qualification>().AsQueryable()
-            .Where(x => x != Qualification.None && (trooperQ.First().Qualifications & x) == x).Select(x => x.AsFull())
-            .ToList();
+        if ((await trooperQ.FirstOrDefaultAsync()) == null)
+            return NotFound(user.ToString());
+        var qualificationsQ = Enum.GetValues<Qualification>().AsQueryable()
+            .Where(x => x != Qualification.None && (trooperQ.First().Qualifications & x) == x).Select(x => x.AsFull());
+        var qualifications = "";
+        if (qualificationsQ.Any())
+             qualifications = qualificationsQ.Aggregate((a, b) => a + ", " + b);
+        var ShopPositionsQ = Enum.GetValues<CShop>().AsQueryable()
+            .Where(x => x != CShop.None && (trooperQ.First().CShops & x) == x).Select(x => x.AsFull());
+        var ShopPositions = "";
+        if (ShopPositionsQ.Any())
+            ShopPositions = ShopPositionsQ.Aggregate((a, b) => a + ", " + b);
         var trooper = trooperQ.Select(e =>
             new FullUserDTO()
             {
@@ -60,28 +80,66 @@ public class LookupController : ControllerBase
                 Rank = e.GetRankName(),
                 Qualifications = qualifications,
                 TIG = (DateTime.Now - e.LastPromotion).Days,
-                TIB = (DateTime.Now - e.LastBilletChange).Days
+                TIB = (DateTime.Now - e.LastBilletChange).Days,
+                CShops = ShopPositions
             }
         ).First();
         
+        string jsonString = JsonSerializer.Serialize(trooper);
 
-        return trooper;
+        await using var sw = new StringWriter();
+        await using var csv = new CsvWriter(sw, CultureInfo.InvariantCulture);
+        csv.WriteHeader<FullUserDTO>();
+        await csv.NextRecordAsync();
+        csv.WriteRecord(trooper);
+        await csv.FlushAsync();
+
+        return sw.ToString();
     }
 
     [HttpGet("billetinfo")]
-    [Produces("application/json")]
-    public async Task<ActionResult<UserDTO[]>> BilletInfo([FromQuery(Name = "enum")] string slot,[FromQuery] string? role)
+    [Produces("text/plain")]
+    public async Task<ActionResult<string>> BilletInfo([FromQuery(Name = "enum")] string slot,[FromQuery] string? role)
     {
         await using var dbContext = _dbContextFactory.CreateDbContext();
         var slotEnum = slot.ValueFromString<Slot>();
-        
+        IEnumerable<UserDTO> members;
         if (role != null)
         {
             var roleEnum = role.ValueFromString<Role>();
-            return dbContext.Users.Where(t => t.Slot == slotEnum && t.Role == roleEnum)
-                .Select(t => new UserDTO() { DisplayName = t.GetRankDesignation() + " " + t.NickName, BirthNumber = t.BirthNumber }).ToArray();
+            members = dbContext.Users.Where(t => t.Slot == slotEnum && t.Role == roleEnum)
+                .Select(t => new UserDTO() { DisplayName = t.GetRankDesignation() + " " + t.NickName, BirthNumber = t.BirthNumber }).AsEnumerable();
         }
-        return dbContext.Users.Where(t => t.Slot == slotEnum)
-            .Select(t => new UserDTO() { DisplayName = t.GetRankDesignation() + " " + t.NickName, BirthNumber = t.BirthNumber }).ToArray();
+        else
+        {
+            members = dbContext.Users.Where(t => t.Slot == slotEnum)
+                .Select(t => new UserDTO()
+                    { DisplayName = t.GetRankDesignation() + " " + t.NickName, BirthNumber = t.BirthNumber }).AsEnumerable();
+        }
+        await using var sw = new StringWriter();
+        await using var csv = new CsvWriter(sw, CultureInfo.InvariantCulture);
+        csv.WriteHeader<UserDTO>();
+        await csv.NextRecordAsync();
+        await csv.WriteRecordsAsync(members);
+        await csv.FlushAsync();
+        await sw.FlushAsync();
+        return sw.ToString();
+    }
+
+    [HttpGet("cshopinfo")]
+    [Produces("text/plain")]
+    public async Task<ActionResult<string>> CShopInfo([FromQuery(Name = "cshop")] string cShopQuery)
+    {
+        await using var dbContext = _dbContextFactory.CreateDbContext();
+        var cShop = cShopQuery.ValueFromString<CShop>();
+        var members = dbContext.Users.Where(t => (t.CShops & cShop) == cShop).Select(t => new UserDTO() { DisplayName = t.GetRankDesignation() + " " + t.NickName, BirthNumber = t.BirthNumber });
+        await using var sw = new StringWriter();
+        await using var csv = new CsvWriter(sw, CultureInfo.InvariantCulture);
+        csv.WriteHeader<UserDTO>();
+        await csv.NextRecordAsync();
+        await csv.WriteRecordsAsync(members);
+        await csv.FlushAsync();
+        await sw.FlushAsync();
+        return sw.ToString();
     }
 }
