2a67e149221c71691a1910aac0943f9b4043f4bf
Hobnob <will@willhobson.co.uk>
2023-04-20 02:13:14 +0100

    fixed EPAR form + Typo fixed "You are currently edtiing" typo

---
+++ FiveOhFirstDataCore.Core/Migrations/20230420011139_Dev-Hob-EPAR-NCORanks.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FiveOhFirstDataCore.Data.Migrations
+{
+    public partial class DevHobEPARNCORanks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NCORank",
+                table: "ChangeRequests",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NCORank",
+                table: "ChangeRequests");
+        }
+    }
+}
